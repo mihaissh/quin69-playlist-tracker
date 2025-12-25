949f6c6f2108741a1c6fa8a40841ace4213ca017
mihaissh <painesih2o@gmail.com>
2025-12-25 17:45:17 +0200

    waterfall improv

--- src/constants/emotes.ts
+++ src/constants/emotes.ts
@@ -1,5 +1,6 @@
-// All 13 emotes available (8 original + 5 new)
+// All emotes available (original + new numbered + new variants)
 const ALL_EMOTES = [
+  // Original size variants
   'clown-tiny.avif',
   'clown-small.avif',
   'clown-medium-1.avif',
@@ -8,6 +9,21 @@ const ALL_EMOTES = [
   'clown-large-2.avif',
   'clown-large-3.avif',
   'clown-huge.avif',
+  // New numbered emotes
+  'clown 1.avif',
+  'clown 2.avif',
+  'clown 3.avif',
+  'clown 4.avif',
+  'clown 5.avif',
+  'clown 6.avif',
+  'clown 7.avif',
+  'clown 8.avif',
+  'clown 9.avif',
+  'clown 10.avif',
+  'clown 11.avif',
+  'clown 12.avif',
+  'clown 13.avif',
+  // New variant emotes
   'clown-new-1.avif',
   'clown-new-2.avif',
   'clown-new-3.avif',
